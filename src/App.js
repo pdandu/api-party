e1873a796c677ea0d30f5c08cef0a6e7ba633347
Pranathi <pdandu@indiana.edu>
2017-05-30 15:53:54 -0400

    Push to new location on submit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import React, { Component } from 'react';
 import { NavLink, Switch, Route } from 'react-router-dom'
 import './App.css';
+import Github from './Github'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <div className="App-headings">
-            <h3>Ain't no party like an</h3>
-            <h1>API Party</h1>
-            </div>
-          <ul className="nav-links">
-            <li>
-              <NavLink to='/github'>GitHub API</NavLink>
-            </li>
-            <li>
-              <NavLink to='/nasa'>Nasa API</NavLink>
-            </li><li>
-              <NavLink to='/homework'>Homework</NavLink>
-            </li>
-          </ul>
-        </div>
-        <Switch>
-          <Route path='/github' render={() => <h1>GITHUB IS AWESOME</h1>} />
-          <Route render={() => <p>To get started, click one of the links above</p>} />
-        </Switch>
-      </div>
-    );
-  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <div className="App-headings">
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to='/github'>Github API</NavLink>
+            </li>
+            <li>
+              <NavLink to='/nasa'>NASA API</NavLink>
+            </li>
+            <li>
+              <NavLink to='/homework'>Homework API</NavLink>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <Route path='/github' component={ Github } />
+          <Route render={() => <p>To get started, click on one of the links above</p>} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Github.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import './Github.css'
+import { Route } from 'react-router-dom'
+
+class Github extends Component {
+  state = {
+    username: ''
+  }
+
+  handleChange = (ev) => {
+    const username = ev.currentTarget.value
+    this.setState({ username })
+  }
+
+  handleSubmit = (ev) => {
+      ev.preventDefault()
+      this.props.history.push(`/github/${this.state.username}`)
+  }
+
+  render() {
+    return (
+      <div className="github">
+        <img className="github-logo" src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github" />
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input 
+              type="text"
+              value={this.state.username}
+              onChange={this.handleChange} />
+          </div>
+          <div>
+            <button type="submit">Look up github user</button>
+          </div>
+        </form>
+
+        <Route exact path='github' render={() => (
+            <h3> Please enter a username to search on Github </h3>
+            )} />
+      </div>
+    )
+  }
+}
+
+export default Github
