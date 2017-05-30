c07a0cab66996f1b15582c8e82572cfa69dd7495
Pranathi <pdandu@indiana.edu>
2017-05-30 15:28:53 -0400

    Add route for GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
         <div className="App-header">
           <div className="App-headings">
             <h3>Ain't no party like an</h3>
-            <h1>API Party </h1>
+            <h1>API Party</h1>
             </div>
           <ul className="nav-links">
             <li>
@@ -22,7 +22,10 @@ class App extends Component {
             </li>
           </ul>
         </div>
-        
+        <Switch>
+          <Route path='/github' render={() => <h1>GITHUB IS AWESOME</h1>} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
+        </Switch>
       </div>
     );
   }
